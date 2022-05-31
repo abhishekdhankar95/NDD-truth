e0479c80ef2999312feb36721e8de0112230c9f1
Abhishek Dhankar <dhankar@ualberta.ca>
2022-05-31 15:16:53 -0600

    deep update

--- general_predictions.py
+++ general_predictions.py
@@ -1,8 +1,10 @@
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.models import Model
 from tensorflow.keras import Sequential
-from non_pubmed_model_training import create_model, embed_text
+from non_pubmed_model_training import create_model, embed_text, train_general_model
 from constants import global_constants
+from extract_text import extract
+import os
 
 glob_c = global_constants()
 
@@ -12,15 +14,19 @@ def predictions(model_name: str, text_list: list):
     model.load_weights(glob_c.model_dir+model_name)
     x_pred = []
     y_pred = []
-    embed_text(x_pred, y_pred, text_list, [])
-    y_pred = model.predict()
+    x_pred, _ = embed_text(x_pred, y_pred, text_list, [])
+    y_pred = model.predict(x_pred)
     return y_pred
 
-'''
-def test(url):
-    text_list = []
-    extract([url], text_list)
-    pred = predictions("general_model", text_list)
-    return pred[0]
-'''
-    
+def test_model(url):
+  if "general_model_weights.index" not in os.listdir(glob_c.model_dir+"general_model/"):
+    train_general_model()
+  text_list = []
+  extract([url], text_list)
+  
+  pred = predictions("general_model/general_model_weights", text_list)
+  prediction_label = glob_c.false_news_label_name
+  if pred[0][0]>0.008:
+    prediction_label = glob_c.true_news_label_name
+  print("prediction: ", prediction_label, "url: ", url, "pred: ", pred[0][0])
+  return (url, prediction_label, pred[0][0])
