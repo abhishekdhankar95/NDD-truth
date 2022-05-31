f76253592b26a182fcf25f9ce891579c901b0f8a
Abhishek Dhankar <dhankar@ualberta.ca>
2022-05-31 15:17:39 -0600

    deep update

--- constants.py
+++ constants.py
@@ -1,6 +1,8 @@
 
 class global_constants():
     def __init__(self):
-        self.model_dir = 'models/'
-        self.data_dir = 'data/'
-        self.prediction_dir = 'predictions/'
+      self.model_dir = 'models/'
+      self.data_dir = 'data/'
+      self.prediction_dir = 'predictions/'
+      self.true_news_label_name = "true_news"
+      self.false_news_label_name = "false_news"
