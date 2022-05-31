f0bfbd5e7ad3fe9aa93e264930dbff28d30f578a
Abhishek Dhankar <dhankar@ualberta.ca>
2022-05-31 13:06:17 -0600

    __init__

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 
 class global_constants():
-    def __init__():
+    def __init__(self):
         self.model_dir = 'models/'
         self.data_dir = 'data/'
         self.prediction_dir = 'predictions/'
