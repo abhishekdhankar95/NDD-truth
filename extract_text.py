3fe3498282da586427e983dbc71369fad531b9e8
Abhishek Dhankar <dhankar@ualberta.ca>
2022-05-31 15:17:18 -0600

    deep update

--- extract_text.py
+++ extract_text.py
@@ -1,14 +1,17 @@
 from newspaper import Article
 from constants import global_constants
 import pandas as pd
+from pickle import dump, load
+import requests
+from newspaper import fulltext
 
 glob_c = global_constants()
 
 def extract(webpage_url_list: list, text_list: list):
     for url in webpage_url_list:
-        article = Article(webpage_url_list)
-        article.download()
-        text_list.append(article)
+        html = requests.get(url).text
+        text = fulltext(html)
+        text_list.append(text)
     
     dump(text_list, open(glob_c.data_dir+'text_list.p', 'wb'))
     
